4ede3090af22c00eb7f3c5df1cb98c9963d752e0
Phil Kunz <gitlab@philkunz.com>
2020-06-01 20:48:54 +0000

    fix(core): update

--- ts/tstest.plugins.ts
+++ ts/tstest.plugins.ts
@@ -1,14 +1,19 @@
 // node native
 import * as path from 'path';
 
+export { path };
+
 // @pushrocks scope
 import * as consolecolor from '@pushrocks/consolecolor';
+import * as smartbrowser from '@pushrocks/smartbrowser';
 import * as smartfile from '@pushrocks/smartfile';
 import * as smartlog from '@pushrocks/smartlog';
 import * as smartpromise from '@pushrocks/smartpromise';
 import * as smartshell from '@pushrocks/smartshell';
 
+export { consolecolor, smartfile, smartlog, smartpromise, smartshell };
+
 // sindresorhus
 import * as figures from 'figures';
 
-export { consolecolor, figures, path, smartfile, smartlog, smartpromise, smartshell };
+export { figures };
