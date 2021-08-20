224400dcb548ea978b2310918acdd916a310cbf2
Philipp Kunz <gitlab@philkunz.com>
2021-08-20 18:25:47 +0200

    fix(core): update

--- ts/index.ts
+++ ts/index.ts
@@ -1,6 +1,10 @@
 import { TsTest } from './tstest.classes.tstest';
 
 export const runCli = async () => {
+  if (!process.argv[2]) {
+    console.error('You must specify a test directory as argument. Please try again.');
+    process.exit(1);
+  }
   const tsTestInstance = new TsTest(process.cwd(), process.argv[2]);
   await tsTestInstance.run();
 };
