eeba113e0971fb3cfd7bf4d7ee671e7c25b10649
Philipp Kunz <gitlab@philkunz.com>
2022-05-04 18:58:35 +0200

    fix(core): update

---
+++ ts/00_commitinfo_data.ts
@@ -0,0 +1,8 @@
+/**
+ * autocreated commitinfo by @pushrocks/commitinfo
+ */
+export const commitinfo = {
+  name: '@gitzone/tstest',
+  version: '1.0.71',
+  description: 'a test utility to run tests that match test/**/*.ts'
+}
